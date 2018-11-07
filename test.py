bcd0f3f8c64c37bfa256000a000acc2d8d30ecd3
Simon Streib <s.d.streib@tudelft.nl>
2018-11-07 13:14:45 +0100

    script file

--- test.py
+++ test.py
@@ -3,3 +3,7 @@ import numpy as np
 def circle_circumference(r):
     """The function takes the radius of a circle and calculates its circumference"""
     return 2*np.pi*r
+
+def circle_area(r):
+    """circle_area(r) return the area of a circle of radius r"""
+    return np.pi*r**2
